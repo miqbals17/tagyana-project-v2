561fb568cf3dcf2e83170e9c1ea32238150380f3
Muhammad Iqbal S <yakuzuyoa@gmail.com>
2025-09-05 21:35:52 +0700

    fix: add eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,81 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+// @ts-check
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+import eslint from "@eslint/js";
+import eslintPluginImport from "eslint-plugin-import";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+    ignores: [
+      "*", // Ignore everything except the following
+      "!public/",
+      "!src/",
+      "!tsconfig.json",
+      "!eslint.config.mjs",
     ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
   },
-])
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx,js,mjs}"],
+    plugins: {
+      import: eslintPluginImport,
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": ["warn"],
+      "@typescript-eslint/no-non-null-assertion": ["warn"],
+      "@typescript-eslint/no-empty-object-type": [
+        "warn",
+        {
+          allowObjectTypes: "always",
+        },
+      ],
+      "@typescript-eslint/explicit-function-return-type": [
+        "warn",
+        {
+          allowExpressions: true,
+          allowTypedFunctionExpressions: true,
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        {
+          prefer: "type-imports",
+          disallowTypeAnnotations: false,
+        },
+      ],
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+            "type",
+          ],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-debugger": "warn",
+      "no-duplicate-imports": "error",
+      "no-unused-expressions": "error",
+    },
+  }
+);
